042f71ddf94e6f2704a2993f520b0ec4696c4985
unknown <joashvaz.97@gmail.com>
2020-10-23 07:00:18 -0700

    my second commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Define UI Vars
 const form = document.querySelector('#task-form');
+// Joash
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
